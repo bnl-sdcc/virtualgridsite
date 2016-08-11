5381fc5099f6de9dce2fdd7420975751aeb28d24
Jose Caballero <jcaballero@bnl.gov>
2016-08-11 16:31:32 -0400

    started creating a real project

---
+++ setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+
+import commands
+import os
+import re
+import sys
+
+from distutils.core import setup
+from distutils.command.install import install as install_org
+from distutils.command.install_data import install_data as install_data_org
+
+
+from virtualgridsite import core
+release_version = core.__version__ 
+
+# ===========================================================
+#                D A T A     F I L E S 
+# ===========================================================
+
+
+etc_files = ['etc/nova.conf',
+             'etc/images.conf',
+             'etc/flavors.conf',
+            ]
+etc_condor_files = ['etc/condor/99-nova.config',]
+etc_condorce_files = ['etc/condor-ce/99-nova.config',]
+
+
+
+
+rpm_data_files=[('/etc/virtualgridsite', etc_files),
+                ('/etc/condor/config.d', etc_condor_files),
+                ('/etc/condor-ce/config.d', etc_condorce_files),
+               ]
+
+
+# -----------------------------------------------------------
+
+def choose_data_files():
+    rpminstall = True
+    userinstall = False
+     
+    if 'bdist_rpm' in sys.argv:
+        rpminstall = True
+
+    elif 'install' in sys.argv:
+        for a in sys.argv:
+            if a.lower().startswith('--home'):
+                rpminstall = False
+                userinstall = True
+                
+    return rpm_data_files
+       
+# ===========================================================
+
+# setup for distutils
+setup(
+    name="virtualgridsite",
+    version=release_version,
+    description='virtualgridsite package',
+    long_description='''This package contains virtualgridsite''',
+    license='GPL',
+    author='Jose Caballero',
+    author_email='jcaballero@bnl.gov',
+    maintainer='Jose Caballero',
+    maintainer_email='jcaballero@bnl.gov',
+    url='https://github.com/jose-caballero/virtualgridsite',
+    # we include the test/ subdirectory
+    packages=['virtualgridsite',
+              ],
+
+    scripts = [ ],
+    
+    data_files = choose_data_files()
+)
